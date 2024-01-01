7f19f5421d52973ac9c6368d6dc211860b7cd67a
Intuition Labs <138280772+intuitonlabs@users.noreply.github.com>
2024-01-01 17:20:47 +0200

    Update Order.java

--- examples/model/Order.java
+++ examples/model/Order.java
@@ -4,7 +4,7 @@ public class Order {
     ....
     /**
      * Add String for paymentLinkId from stripe service
-     * ⚠ Make sure to update OrderServiceImpl.java as adding the propartie to update mathod.
+     * ⚠ Make sure to update OrderServiceImpl.java as adding the property to update mathod.
      */
     private String paymentLinkId;
     ....
